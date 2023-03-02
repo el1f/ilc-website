9b980165409a342e632388ee246e00c27c401d87
Ayoub Aabass <ayoub@aabass.net>
2023-03-02 15:53:17 +0100

    base setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+import "../styles/globals.css";
+import { Space_Grotesk } from "next/font/google";
+
+const thunder = localFont({
+	src: "../fonts/thunder/thunder-vf.ttf",
+	variable: "--font-thunder",
+});
+
+const spaceGrotesk = Space_Grotesk({
+	subsets: ["latin"],
+	variable: "--font-space-grotesk",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className={`${thunder.variable} ${spaceGrotesk.variable} font-sans`}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['var(--font-thunder)'],
+        sans: ['var(--font-space-grotesk)'],
+      }
+    },
+  },
+  plugins: [],
+}
